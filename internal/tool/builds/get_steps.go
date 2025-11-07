e9ee474733e9ff02b475ac8257fdcd5a166742d5
Szabolcs Gelencsér <szabolcs.gelencser@bitrise.io>
2025-11-07 18:07:12 +0100

    feat: enable getting structured build logs

---
+++ internal/tool/builds/get_steps.go
@@ -0,0 +1,45 @@
+package builds
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/bitrise-io/bitrise-mcp/v2/internal/bitrise"
+	"github.com/mark3labs/mcp-go/mcp"
+)
+
+var GetSteps = bitrise.Tool{
+	APIGroups: []string{"builds", "read-only"},
+	Definition: mcp.NewTool("get_build_steps",
+		mcp.WithDescription("Get step statuses of a specific build of a given app."),
+		mcp.WithString("app_slug",
+			mcp.Description("Identifier of the Bitrise app"),
+			mcp.Required(),
+		),
+		mcp.WithString("build_slug",
+			mcp.Description("Identifier of the build"),
+			mcp.Required(),
+		),
+	),
+	Handler: func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+		appSlug, err := request.RequireString("app_slug")
+		if err != nil {
+			return mcp.NewToolResultError(err.Error()), nil
+		}
+		buildSlug, err := request.RequireString("build_slug")
+		if err != nil {
+			return mcp.NewToolResultError(err.Error()), nil
+		}
+
+		res, err := bitrise.CallAPI(ctx, bitrise.CallAPIParams{
+			Method:  http.MethodGet,
+			BaseURL: bitrise.APIBaseURL,
+			Path:    fmt.Sprintf("/apps/%s/builds/%s/log/summary", appSlug, buildSlug),
+		})
+		if err != nil {
+			return mcp.NewToolResultErrorFromErr("call api", err), nil
+		}
+		return mcp.NewToolResultText(res), nil
+	},
+}
