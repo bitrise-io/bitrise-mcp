1892de7925a3ffe9a31cd382ed40ad5dc0010a0d
Márton Molnár <marton.molnar@bitrise.io>
2026-02-11 10:05:44 +0100

    nit: cvs -> step_ref

--- internal/tool/configuration/step_inputs.go
+++ internal/tool/configuration/step_inputs.go
@@ -12,9 +12,9 @@ var StepInputs = bitrise.Tool{
 	APIGroups: []string{"configuration", "read-only"},
 	Definition: mcp.NewTool("step_inputs",
 		mcp.WithDescription("List inputs of a step with their defaults, allowed values etc."),
-		mcp.WithString("cvs",
+		mcp.WithString("step_ref",
 			// Step reference format: https://docs.bitrise.io/en/bitrise-ci/references/steps-reference/step-reference-id-format.html
-			mcp.Description("Step reference formatted as `step_lib_source::step_id@version`. Only `step_id` is required, `version` should be added when known, `step_lib_source` is only necessary for custom step sources."),
+			mcp.Description("Step reference formatted as `step_lib_source::step_id@version`. `step_id` and an exact `version` are required, `step_lib_source` is only necessary for custom step sources."),
 			mcp.Required(),
 		),
 		mcp.WithReadOnlyHintAnnotation(true),
@@ -23,7 +23,7 @@ var StepInputs = bitrise.Tool{
 		mcp.WithIdempotentHintAnnotation(true),
 	),
 	Handler: func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
-		query, err := request.RequireString("cvs")
+		query, err := request.RequireString("step_ref")
 		if err != nil {
 			return mcp.NewToolResultError(err.Error()), nil
 		}
@@ -32,7 +32,7 @@ var StepInputs = bitrise.Tool{
 			Method:  http.MethodGet,
 			BaseURL: bitrise.APIBaseURL,
 			Path:    "/step-inputs",
-			Params:  map[string]any{"cvs": query},
+			Params:  map[string]any{"step_ref": query},
 		})
 		if err != nil {
 			return mcp.NewToolResultErrorFromErr("call api", err), nil
